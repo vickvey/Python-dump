4a2c4041c0328507e8f82fbb4c51a9e1de6ba330
vickvey <vivek22@iiserb.ac.in>
2023-12-07 23:46:07 +0530

    added learn-python folder and some resources

---
+++ learn-python/03_variables.py
@@ -0,0 +1,17 @@
+# Variables in python
+
+# Variables are created when you assign a value to it
+x = 5
+y = 'Hello World'
+
+# Method to assign multiple variables
+p, q, r = "Apples", "Bananas", "Coconuts"
+
+# Assign same value to multiple variables
+a = b = c = 'Python'
+
+
+# Make the variable global
+def myfunc():
+    global x
+    x = "fantastic"
---
+++ learn-python/main.py
@@ -0,0 +1,6 @@
+# Learn python
+
+fruits = ['apples', 'bananas', 'pineapples', 'kiwis']
+for i in fruits:
+    print(i, sep=' ')
+print('\n')
